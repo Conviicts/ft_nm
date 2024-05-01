b8254f2ef77b607855512226105cd646e97db9c0
jode-vri <jode-vri@student.42lyon.fr>
2024-05-01 10:26:41 +0200

    .

--- incs/ft_nm.h
+++ incs/ft_nm.h
@@ -5,8 +5,21 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdbool.h>
 
 #include "libft.h"
+
+typedef struct s_file {
+    char    *name;
+    char    *error;
+    int     fd;
+}   t_file;
+
+typedef struct s_nm {
+    t_list  *files;
+}   t_nm;
 
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -12,9 +12,45 @@
 
 #include "ft_nm.h"
 
+void	parser(t_nm *nm, int ac, char **av) {
+	t_file	tmp;
+	t_stat	st;
+	int		fd;
+	for (int i = 1; i < ac; i++) {
+		if ((fd = open(av[i], 0)) == -1) {
+			tmp.error = ft_strjoin("ft_nm: '", av[i]);
+			tmp.error = ft_strjoin_free(tmp.error, "': No such file\n");
+
+		}
+		tmp.name = av[i];
+		tmp.fd = fd;
+		if (lstat(av[i], &st) > -1)
+			ft_lstadd_back(&nm->files, ft_lstnew((void *)&tmp));
+		else {
+			tmp.error = ft_strjoin("ft_nm: ", av[i]);
+			tmp.error = ft_strjoin_free(tmp.error, ": Permission denied\n");
+		}
+	}
+}
+
 int		main(int ac, char **av) {
-	(void)ac;
-	(void)av;
+	t_nm	nm;
+	// t_list	*tmp;
+
+	if (ac == 1)
+		return (1);
+	ft_bzero(&nm, sizeof(nm));
+	parser(&nm, ac, av);
+
+	// printf("%s\n", ((t_file *)nm.files->content)->name);
+	// tmp = nm.files;
+	// while (tmp) {
+	// 	// if (((t_file *)tmp->content)->error) {
+	// 	// 	printf("%s", ((t_file *)tmp->content)->error);
+	// 	// }
+	// 	tmp = tmp->next;
+	// }
+
 
 	return (0);
 }
